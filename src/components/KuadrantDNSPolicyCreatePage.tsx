ba0b039598388db85e776631ce1ea35ddbdd54ad
David Martin <davmarti@redhat.com>
2024-09-03 10:57:59 +0100

    Add namespace selector
    Signed-off-by: David Martin <davmarti@redhat.com>

--- src/components/KuadrantDNSPolicyCreatePage.tsx
+++ src/components/KuadrantDNSPolicyCreatePage.tsx
@@ -4,12 +4,46 @@ import {
   Page,
   PageSection,
   Title,
+  TextInput,
+  FormGroup,
+  FormHelperText,
+  HelperText,
+  HelperTextItem,
+  Form,
+  FormSelect,
+  FormSelectOption,
 } from '@patternfly/react-core';
 import { useTranslation } from 'react-i18next';
 import './kuadrant.css';
+import { useK8sWatchResource } from '@openshift-console/dynamic-plugin-sdk';
 
 const KuadrantDNSPolicyCreatePage: React.FC = () => {
   const { t } = useTranslation('plugin__console-plugin-template');
+  const [policy, setPolicy] = React.useState('');
+  const handlePolicyChange = (_event, policy: string) => {
+    setPolicy(policy);
+  };
+
+  const [selectedNamespace, setSelectedNamespace] = React.useState('');
+  const [namespaces, setNamespaces] = React.useState([]);
+
+  const namespaceResource = {
+    kind: 'Namespace',
+    isList: true,
+    namespaced: false,
+  };
+
+  const [namespaceData, loaded, error] = useK8sWatchResource(namespaceResource);
+
+  React.useEffect(() => {
+    if (loaded && !error && Array.isArray(namespaceData)) {
+      setNamespaces(namespaceData.map((ns) => ns.metadata.name));
+    }
+  }, [namespaceData, loaded, error]);
+
+  const handleNamespaceChange = (event) => {
+    setSelectedNamespace(event.currentTarget.value);
+  };
 
   return (
     <>
@@ -23,11 +57,36 @@ const KuadrantDNSPolicyCreatePage: React.FC = () => {
         <div>create dns policy</div>
         <div>description</div>
         <div>create via [] form [] yaml</div>
-        <div>policy name *</div>
-        <input/>
-        <div>unique name of the DNS Policy</div>
-        <div>description</div>
-        <input/>
+        <Form>
+          <FormGroup label="Policy Name" isRequired fieldId="policy-name">
+            <TextInput
+              isRequired
+              type="text"
+              id="policy-name"
+              name="policy-name"
+              value={policy}
+              onChange={handlePolicyChange}
+            />
+            <FormHelperText>
+              <HelperText>
+                <HelperTextItem>Unique name of the DNS Policy.</HelperTextItem>
+              </HelperText>
+            </FormHelperText>
+          </FormGroup>
+          <FormGroup label="Select Namespace" fieldId="namespace-select">
+            <FormSelect
+              id="namespace-select"
+              value={selectedNamespace}
+              onChange={handleNamespaceChange}
+              aria-label="Select Namespace"
+            >
+              <FormSelectOption key="placeholder" value="" label="Select a namespace" isPlaceholder />
+              {namespaces.map((namespace, index) => (
+                <FormSelectOption key={index} value={namespace} label={namespace} />
+              ))}
+            </FormSelect>
+          </FormGroup>
+        </Form>
         <div>namespace * ?</div>
         <input/>
         <div>gateway api target reference *</div>
@@ -51,6 +110,8 @@ const KuadrantDNSPolicyCreatePage: React.FC = () => {
         <div>Protocol * ?</div>
         <div>[] default</div>
         <div>[] custom weights</div>
+        <button>create</button>
+        <button>cancel</button>
       </Page>
     </>
   );
