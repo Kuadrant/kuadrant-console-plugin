e90d6c616341132bda22138d741ad1544723194f
Tommy Condon <tcondon@redhat.com>
2025-07-17 15:08:30 +0100

    Refactor Kuadrant Status Alert, handle undefined case & use tooltip popover
    Signed-off-by: Tommy Condon <tcondon@redhat.com>

--- src/components/KuadrantStatusAlert.tsx
+++ src/components/KuadrantStatusAlert.tsx
@@ -3,9 +3,14 @@ import {
   K8sResourceCommon,
   useK8sWatchResource,
 } from '@openshift-console/dynamic-plugin-sdk';
-import { Alert } from '@patternfly/react-core';
+import { Icon } from '@patternfly/react-core';
 import * as React from 'react';
 import { useTranslation } from 'react-i18next';
+import { CheckCircleIcon, ExclamationTriangleIcon } from '@patternfly/react-icons';
+import resourceGVKMapping from '../utils/latest';
+import { Tooltip } from '@patternfly/react-core';
+import { InfoCircleIcon, ExclamationCircleIcon } from '@patternfly/react-icons';
+
 interface KuadrantResource extends K8sResourceCommon {
   status?: {
     conditions?: {
@@ -19,67 +24,122 @@ interface KuadrantResource extends K8sResourceCommon {
 
 export const KuadrantStatusAlert: React.FC = () => {
   const { t } = useTranslation('plugin__kuadrant-console-plugin');
-  const kuadrantGVK: K8sGroupVersionKind = {
-    group: 'kuadrant.io',
-    version: 'v1beta1',
-    kind: 'Kuadrant',
-  };
+  const kuadrantGVK: K8sGroupVersionKind = resourceGVKMapping.Kuadrant;
 
   const [kuadrantResource, loaded, err] = useK8sWatchResource<KuadrantResource>({
     groupVersionKind: kuadrantGVK,
   });
 
-  if (!err) {
-    if (!loaded) {
-      return <div>{t('Kuadrant Status Loading...')}</div>;
-    } else {
-      const reason = kuadrantResource?.status?.conditions?.[0]?.reason;
+  const conditions = kuadrantResource?.status?.conditions ?? [];
+  const missingDependency = conditions.filter((c) => c.reason === 'MissingDependency');
+  const success = conditions.filter((c) => c.reason === 'Ready');
 
-      if (reason === 'MissingDependency') {
-        return (
-          <Alert
-            isExpandable={true}
-            title={t('Kuadrant Status')}
-            variant="warning"
-            ouiaId="WarningAlert"
-          >
-            <div>
-              {t('Message')}: {kuadrantResource?.status?.conditions?.[0]?.message}
-            </div>
-            <div>
-              {t('Reason')}: {kuadrantResource?.status?.conditions?.[0]?.reason}
-            </div>
-            <div>
-              {t('Status')}: {kuadrantResource?.status?.conditions?.[0]?.status}
-            </div>
-          </Alert>
-        );
-      } else {
-        return (
-          <Alert
-            isExpandable={true}
-            title={t('Kuadrant Status')}
-            variant="success"
-            ouiaId="SuccessAlert"
-          >
-            <div>
-              {t('Message')}: {kuadrantResource?.status?.conditions?.[0]?.message}
-            </div>
-            <div>
-              {t('Reason')}: {kuadrantResource?.status?.conditions?.[0]?.reason}
-            </div>
-            <div>
-              {t('Status')}: {kuadrantResource?.status?.conditions?.[0]?.status}
-            </div>
-          </Alert>
-        );
-      }
-    }
-  } else {
+  if (err) {
     return (
-      <Alert isExpandable={true} title={t('Kuadrant Status')} variant="danger">
-        <div>{t(err.toString())}</div>
-      </Alert>
+      <div style={{ display: 'flex' }}>
+        <Tooltip position="top" content={t(err.toString())}>
+          <div style={{ display: 'flex', alignItems: 'center', gap: '5px' }}>
+            <Icon status="danger">
+              <ExclamationCircleIcon />
+            </Icon>
+            <div>{t('Kuadrant Status')}</div>
+          </div>
+        </Tooltip>
+      </div>
     );
   }
+
+  if (!loaded) {
+    return <div>{t('Kuadrant Status Loading...')}</div>;
+  }
+
+  if (conditions.length === 0) {
+    return (
+      <div style={{ display: 'flex' }}>
+        <Tooltip position="top" content={t('Checking Kuadrant Status...')}>
+          <div style={{ display: 'flex', alignItems: 'center', gap: '5px' }}>
+            <Icon status="info">
+              <InfoCircleIcon />
+            </Icon>
+            <div>{t('Kuadrant Status')}</div>
+          </div>
+        </Tooltip>
+      </div>
+    );
+  }
+
+  if (missingDependency.length > 0) {
+    return (
+      <div style={{ display: 'flex' }}>
+        <Tooltip
+          position="top"
+          content={
+            <div>
+              <div>{t(missingDependency[0].message)}</div>
+              <div>
+                {t('Reason: ')} {missingDependency[0].reason}
+              </div>
+              <div>
+                {t('Status: ')}
+                <Icon status="warning">
+                  <ExclamationTriangleIcon />
+                </Icon>
+              </div>
+            </div>
+          }
+        >
+          <div style={{ display: 'flex', alignItems: 'center', gap: '5px' }}>
+            <Icon status="warning">
+              <ExclamationTriangleIcon />
+            </Icon>
+            <div>{t('Kuadrant Status')}</div>
+          </div>
+        </Tooltip>
+      </div>
+    );
+  }
+
+  if (success.length > 0) {
+    return (
+      <div style={{ display: 'flex' }}>
+        <Tooltip
+          position="top"
+          content={
+            <div>
+              <div>{t(success[0].message)}</div>
+              <div>
+                {t('Reason: ')} {success[0].reason}
+              </div>
+              <div>
+                {t('Status: ')}
+                <Icon status="success">
+                  <CheckCircleIcon />
+                </Icon>
+              </div>
+            </div>
+          }
+        >
+          <div style={{ display: 'flex', alignItems: 'center', gap: '5px' }}>
+            <Icon status="success">
+              <CheckCircleIcon />
+            </Icon>
+            <div>{t('Kuadrant Status')}</div>
+          </div>
+        </Tooltip>
+      </div>
+    );
+  }
+
+  return (
+    <div style={{ display: 'flex' }}>
+      <Tooltip position="top" content={t('Kuadrant status unavailable.')}>
+        <div style={{ display: 'flex', alignItems: 'center', gap: '5px' }}>
+          <Icon status="warning">
+            <ExclamationTriangleIcon />
+          </Icon>
+          <div>{t('Kuadrant status unavailable.')}</div>
+        </div>
+      </Tooltip>
+    </div>
+  );
 };
