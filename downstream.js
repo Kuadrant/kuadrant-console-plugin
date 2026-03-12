ccdd61adc9edd25a478ee4e1fb591ad75fa71eda
Eguzki Astiz Lezaun <eastizle@redhat.com>
2026-03-12 18:47:25 +0100

    kuadrantoverview: fix policy guide link
    Signed-off-by: Eguzki Astiz Lezaun <eastizle@redhat.com>

--- downstream.js
+++ downstream.js
@@ -22,7 +22,7 @@ const replacements = {
       // Direct string replacements for links.ts
       // Order matters: specific URLs first to prevent partial matches
       'https://docs.kuadrant.io/latest/kuadrant-operator/doc/user-guides/full-walkthrough/secure-protect-connect/':
-        `https://docs.redhat.com/en/documentation/red_hat_connectivity_link/${version}/html-single/configuring_and_deploying_gateway_policies_with_connectivity_link/index`,
+        `https://docs.redhat.com/en/documentation/red_hat_connectivity_link/${version}/html/configuring_and_deploying_gateway_policies/index`,
       'https://docs.kuadrant.io/latest/kuadrant-operator/doc/observability/examples/':
         `https://docs.redhat.com/en/documentation/red_hat_connectivity_link/${version}/html-single/connectivity_link_observability_guide/index`,
       'https://docs.kuadrant.io':
