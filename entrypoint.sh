1d6bc0dc26a3308280fe761b9c4975952cfdee4f
dd <4183971+didierofrivia@users.noreply.github.com>
2026-03-16 15:02:31 +0100

    [script] Adding metrics configuration in entrypoint script
    Signed-off-by: dd <4183971+didierofrivia@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-# Inject topology ConfigMap location
+# Inject topology ConfigMap location and metrics configuration
 cat <<EOF > /tmp/config.js
 window.kuadrant_config = {
-  TOPOLOGY_CONFIGMAP_NAME: '${TOPOLOGY_CONFIGMAP_NAME}',
-  TOPOLOGY_CONFIGMAP_NAMESPACE: '${TOPOLOGY_CONFIGMAP_NAMESPACE}'
+  TOPOLOGY_CONFIGMAP_NAME: '${TOPOLOGY_CONFIGMAP_NAME:-topology}',
+  TOPOLOGY_CONFIGMAP_NAMESPACE: '${TOPOLOGY_CONFIGMAP_NAMESPACE:-kuadrant-system}',
+  METRICS: {
+    metricName: '${METRICS_METRIC_NAME:-istio_request_duration_milliseconds_count}',
+    queryFunction: '${METRICS_QUERY_FUNCTION:-rate}',
+    timeWindow: '${METRICS_TIME_WINDOW:-2m}',
+    workloadSuffix: '${METRICS_WORKLOAD_SUFFIX:-}',
+    successCodePattern: '${METRICS_SUCCESS_CODE_PATTERN:-2(.*)|3(.*)}',
+  }
 };
 EOF
 
