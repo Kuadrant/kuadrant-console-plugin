e9b2b20215e977a1563f313967f5ec9c2401cebf
dd <4183971+didierofrivia@users.noreply.github.com>
2026-03-17 15:48:09 +0100

    [refactor] Using `react-router-dom-v5-compat` `useNavigate` instead
    * This should fix the incompatibility raised by the upgrade in OpenShift
      console plugin after merging https://github.com/openshift/console/pull/15956
    
    Signed-off-by: dd <4183971+didierofrivia@users.noreply.github.com>

--- src/utils/cancel.tsx
+++ src/utils/cancel.tsx
@@ -1,5 +1,5 @@
-import { History } from 'history';
+import { NavigateFunction } from 'react-router-dom-v5-compat';
 
-export function handleCancel(namespace: string, data, history: History) {
-  history.goBack();
+export function handleCancel(namespace: string, data, navigate: NavigateFunction) {
+  navigate(-1);
 }
