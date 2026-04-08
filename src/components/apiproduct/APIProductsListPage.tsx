17d5515598173f832ea4c0a8b32bfadea7d98e48
dd <4183971+didierofrivia@users.noreply.github.com>
2026-04-08 12:52:43 +0200

    [feat] Project selector and breadcrumbs
    Signed-off-by: dd <4183971+didierofrivia@users.noreply.github.com>

--- src/components/apiproduct/APIProductsListPage.tsx
+++ src/components/apiproduct/APIProductsListPage.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom-v5-compat';
 import { PageSection, Title } from '@patternfly/react-core';
-import { useActiveNamespace } from '@openshift-console/dynamic-plugin-sdk';
+import { useActiveNamespace, NamespaceBar } from '@openshift-console/dynamic-plugin-sdk';
 import ResourceList from '../ResourceList';
 import { RESOURCES } from '../../utils/resources';
 import '../kuadrant.css';
@@ -9,9 +10,19 @@ import '../kuadrant.css';
 const APIProductsListPage: React.FC = () => {
   const { t } = useTranslation('plugin__kuadrant-console-plugin');
   const [activeNamespace] = useActiveNamespace();
+  const navigate = useNavigate();
+
+  const handleNamespaceChange = (namespace: string) => {
+    if (namespace !== '#ALL_NS#') {
+      navigate(`/kuadrant/ns/${namespace}/apiproducts`, { replace: true });
+    } else {
+      navigate('/kuadrant/all-namespaces/apiproducts', { replace: true });
+    }
+  };
 
   return (
     <>
+      <NamespaceBar onNamespaceChange={handleNamespaceChange} />
       <PageSection hasBodyWrapper={false}>
         <Title headingLevel="h1">{t('API Products')}</Title>
       </PageSection>
