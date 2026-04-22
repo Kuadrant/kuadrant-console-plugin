cedeb877ad6505d0740713406329d14011da1c9f
Eguzki Astiz Lezaun <eastizle@redhat.com>
2026-04-22 17:12:00 +0200

    MyAPIKeysPage: as console.navigation/resource-ns
    Signed-off-by: Eguzki Astiz Lezaun <eastizle@redhat.com>

--- src/components/apikey/MyAPIKeysPage.tsx
+++ src/components/apikey/MyAPIKeysPage.tsx
@@ -1,38 +1,32 @@
 import * as React from 'react';
 import { useTranslation } from 'react-i18next';
-import { useNavigate } from 'react-router-dom-v5-compat';
-import { PageSection, Title } from '@patternfly/react-core';
-import { useActiveNamespace, NamespaceBar } from '@openshift-console/dynamic-plugin-sdk';
-import ResourceList from '../ResourceList';
-import { RESOURCES } from '../../utils/resources';
+import { PageSection, Title, EmptyState, EmptyStateBody } from '@patternfly/react-core';
+import { ListPageBody } from '@openshift-console/dynamic-plugin-sdk';
+//import { useActiveNamespace } from '@openshift-console/dynamic-plugin-sdk';
+import { SearchIcon } from '@patternfly/react-icons';
 import '../kuadrant.css';
 
 const MyAPIKeysPage: React.FC = () => {
   const { t } = useTranslation('plugin__kuadrant-console-plugin');
-  const [activeNamespace] = useActiveNamespace();
-  const navigate = useNavigate();
-
-  const handleNamespaceChange = (namespace: string) => {
-    if (namespace !== '#ALL_NS#') {
-      navigate(`/kuadrant/ns/${namespace}/myapikeys`, { replace: true });
-    } else {
-      navigate('/kuadrant/all-namespaces/myapikeys', { replace: true });
-    }
-  };
+  //const [activeNamespace] = useActiveNamespace();
 
   return (
     <>
-      <NamespaceBar onNamespaceChange={handleNamespaceChange} />
       <PageSection hasBodyWrapper={false}>
         <Title headingLevel="h1">{t('My API Keys')}</Title>
       </PageSection>
-      <PageSection hasBodyWrapper={false} className="kuadrant-policy-list-body">
-        <ResourceList
-          resources={[RESOURCES.APIKeyRequest.gvk]}
-          namespace={activeNamespace}
-          emptyResourceName="API Keys"
-        />
-      </PageSection>
+      <ListPageBody>
+        <EmptyState
+          titleText={
+            <Title headingLevel="h4" size="lg">
+              {t('No API Keys found')}
+            </Title>
+          }
+          icon={SearchIcon}
+        >
+          <EmptyStateBody>t('No API Keys match the filter criteria.')</EmptyStateBody>
+        </EmptyState>
+      </ListPageBody>
     </>
   );
 };
